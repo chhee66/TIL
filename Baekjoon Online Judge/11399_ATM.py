e6b3f456f88790194da7c1a98aaa384a3ab27bfa
Chohui Park <chhee66@naver.com>
2019-12-19 19:07:01 +0900

    [EDIT] string P[] -> int P[]
    P를 input().split()으로 받아서 문자열인 상태에서 .sort()를 하니
    N=10
    P = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 일 때,
    (sort 후) P = 1, "10", 2, 3, 4, 5, 6, 7, 8, 9
    로 예상치 못한 결과가 있었다.
    
    따라서 처음부터 P를 map()을 통해 int형으로 바꾸어준 뒤 sort()함수를 사용하여 문제를 해결하였다.
    
    백준 채점 결과 : CORRECT

--- Baekjoon Online Judge/11399_ATM.py
+++ Baekjoon Online Judge/11399_ATM.py
@@ -5,12 +5,15 @@ N = int(input())
 if N==0 :
     print('0')
 else:
-    P = input().split()     # Python에서 사라질 함수 : atoi() - 따라서 int('string'자료형 사용), float()를 사용
+    P = list(map(int, input().split()))    # input().split()으로 받은 후 한 원소마다 int()로 바꾸지 말고, map()을 사용해서 한 줄로 해결!
     P.sort()
-    Q = [int(P[0])]
-    sum=int(P[0])
+    Q = [P[0]]
+    sum=P[0]
     for i in range(1, N):
-        Q.append(Q[i-1]+int(P[i]))
+        Q.append(Q[i-1]+P[i])
         sum = sum+Q[i]
 
     print(sum)
+    
+    
+# Python에서 사라질 함수 : atoi() - 따라서 int('string'자료형 사용), float()를 사용
