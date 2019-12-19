38e69796e9cdc2448a62b321808b078c3c200bb5
Chohui Park <chhee66@naver.com>
2019-12-19 18:59:09 +0900

    [EDIT] N=0 일 때 조건 추가
    N이 0이면 기다리는 사람이 없으므로 0을 출력한다.
    
    백준 채점 결과 : FAILED

--- Baekjoon Online Judge/11399_ATM.py
+++ Baekjoon Online Judge/11399_ATM.py
@@ -2,12 +2,15 @@
 # 20191219
 
 N = int(input())
-P = input().split()     # Python에서 사라질 함수 : atoi() - 따라서 int('string'자료형 사용), float()를 사용
-P.sort()
-Q = [int(P[0])]
-sum=int(P[0])
-for i in range(1, N):
-    Q.append(Q[i-1]+int(P[i]))
-    sum = sum+Q[i]
+if N==0 :
+    print('0')
+else:
+    P = input().split()     # Python에서 사라질 함수 : atoi() - 따라서 int('string'자료형 사용), float()를 사용
+    P.sort()
+    Q = [int(P[0])]
+    sum=int(P[0])
+    for i in range(1, N):
+        Q.append(Q[i-1]+int(P[i]))
+        sum = sum+Q[i]
 
-print(sum)
+    print(sum)
