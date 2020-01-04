e3969a4ad5495822c3915fb3126f81cb0d74a2e0
Chohui Park <chhee66@naver.com>
2020-01-04 21:22:16 +0900

    [EDIT] change the method
    이전에는 전체 수 중 가장 작은 수를 가진 집을 찾아서, 그 집을 기준으로 위아래로 영역을 넓혀가며 이어서 작은 수를 찾아내는 방법을 생각함.  이후 @wlsl8135 에게 풀이방법을 듣고 한 줄씩 내려가면서 그 앞 집의 가격 중 안 겹치되 가장 작은 수를 더해가는 방법으로 바꿈. 하지만 "틀렸다"

--- Baekjoon Online Judge/1149_RGB distance.py
+++ Baekjoon Online Judge/1149_RGB distance.py
@@ -1,26 +1,28 @@
 # 1149번 : RGB거리
 # 20200103
 
+def find_neighbor(i, j):    # 함수를 사용하기 전에 정의되어있어야한다. (메인소스코드의 하단 말고 상단에 위치시키기!)
+    min = 1001
+    
+    for k in range(0, 3):
+        
+        if k==j :
+            continue
+        else :
+            if min>price[i-1][k]:
+                min = price[i-1][k]
+    return min
+
+
 N = int(input())
 price = []
 for i in range(0, N):
     data = list(map(int, input().split()))
     price.append(data)
 
-b = price[:]
-for j in range(0, N):
-    b[j].sort()
-b.sort()
-min = b[0][0]
+for i in range(1, N) :
+    for j in range(0, 3):
+        price[i][j] = price[i][j] + find_neighbor(i, j)
 
-for k in range(0, N):
-    idx=-1
-    try:
-        idx = price[k].index(min)
-    except IndexError :
-        continue
-    finally:
-        if idx is not -1:
-            break
-    
-price[k][idx]=-1    
+price[N-1].sort()
+print(price[N-1][0])
