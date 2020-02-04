1931600e44268576564378508e453d5bf71cde74
Chohui Park <chhee66@naver.com>
2020-02-04 19:48:40 +0900

    [UPLOAD] 1260_DFS and BFS
    wrong solution

---
+++ Baekjoon Online Judge/1260_DFS and BFS.py
@@ -0,0 +1,19 @@
+data = list(map(int, input().split()))
+N = data[0]
+M = data[1]
+V = data[2]
+
+key=-1
+table = list()
+for i in range(0, M):
+    value = list(map(int, input().split()))
+    key=value[0]
+    if key!=value[0]:
+    else :
+        
+        
+    
+    
+
+
+
