4426b5205ee817d54ddb0bceaa0c39e6d308884b
Chohui Park <chhee66@naver.com>
2020-05-08 23:04:02 +0900

    [EDIT] Done with DFS and BFS is yet
    Disunite same codes to independent function. (delete_from_list, search_exist_list)
    
    And I have to revise the codes of BFS part.
    
    * Dictionary *
     - disunite : 분리하다
     - revise : 수정하다

--- Baekjoon Online Judge/1260_DFS and BFS.py
+++ Baekjoon Online Judge/1260_DFS and BFS.py
@@ -3,68 +3,81 @@ N = data1[0]
 M = data1[1]
 V = data1[2]
 
-dict = {}
+DFS = {}
+BFS = {}
 
 for i in range(1, N+1):
-    dict[i] = list()
+    DFS[i] = list()
+    BFS[i] = list()
     
 for j in range(0, M):
     data2 = list(map(int, input().split()))
-    dict[data2[0]].append(data2[1])
-    dict[data2[1]].append(data2[0])
-
-# print(dict) #COMPLETE
+    DFS[data2[0]].append(data2[1])
+    DFS[data2[1]].append(data2[0])
+    BFS[data2[0]].append(data2[1])
+    BFS[data2[1]].append(data2[0])
 
 # DFS = BFS = dict # 모두 같은 id를 가지는 복사 방법. 하나를 변경하면 모두 똑같이 적용됨.
-
+'''
 # dictionary를 다른 id로 복사하는 방법
+# (+) 다른 id로 복사했는데도 서로 영향을 미친다.
 DFS = {}
 BFS = {}
 for k, v in dict.items():
     DFS[k]=v
     BFS[k]=v
-
 '''
-############### DFS ###############
-# 재귀함수? 반복문(모든 딕셔너리의 원소가 -1이 참일 때까지)?
-key=V
-print(key, end='')
-for k in range(0, N-1):
-    for l in range(1, N+1):
-        if key in DFS[l]:
-            DFS[l].remove(key)
-    
-    key=min(DFS[key])
-    print(' '+str(key), end='')  # 한 줄로 출력하기 end='' 는 띄어쓰기 1번 / end=' ' 는 띄어쓰기 2번
-                            # print()에서 ','는 띄어쓰기 자동추가, '+'는 띄어쓰기 없이 출력한다
-# Q1. 마지막으로 출력되는 원소가 리스트에 남는다. 모두 빈 리스트인 상태로 종료하고싶다면 어떻게 해야할까?
 
-print()
+# 공통 함수_1
+def delete_from_list(list_name, num):
+    for i in range(1, N+1):
+        if num in list_name[i]:
+            list_name[i].remove(num)
+
+# 공통 함수_2
+def search_exist_list(list_name):
+    count = 0
+    for i in range(1, N+1):
+        if list_name[i]:
+            return i
+        else :
+            count = count+1
+    if count==N :
+        return -1 #전체 종료
+            
+
+############### DFS ############### (출력은 되는데, 뒤에 남는 빨간 줄은 뭐지..?)
+
+def DFS_(first):
+    main_key = first
+    delete_from_list(DFS, main_key)
+    if DFS[main_key]:
+        return min(DFS[first])
+    else :
+        search_exist_list(DFS)
+
+
+key=V
+while (DFS_(key)) :
+    print(key, end=' ')
+    key = DFS_(key)
+print(key, end=' ')
+
 '''
 ############### BFS ###############
 
-def delete_from_list(key, value):
-    for l in range(1, N+1):
-        if l==key:
-            BFS[l] = list()
-        elif value in BFS[l]:
-            BFS[l].remove(value)
-        
-
+def BFS_(first):
+    delete_from_list(BFS, first)
+    if BFS[first]:
+        for j in BFS[first]:
+            print(min(BFS[first]), end=' ')
+            delete_from_list(BFS, min(BFS[first]))
+        return first+1
+    else :        
+        search_exist_list(BFS)
 
 key=V
 print(key, end=' ')
-delete_from_list(key, key)
-while(1):
-    if BFS[key] :
-        for n in range(0, len(BFS[key])):
-            print(min(BFS[key]), end=' ')
-            delete_from_list(key, min(BFS[key]))
-    else:
-        for m in range(1, N+1):
-            if BFS[m] :
-                key = m
-                print(str(key), end=' ')
-                delete_from_list(key, key)
-            else:
-                exit()
+while (key != -1) :
+    key = BFS_(key)
+'''
