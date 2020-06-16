c3e0cfdfc1912d0aebfdd1560a6d64aa6d7c9963
Chohui Park <chhee66@naver.com>
2020-06-16 18:00:24 +0900

    [EDIT] BFS is running correctly!
    간선의 연결성 여부를 확인수 있는 바둑판 모양의 이차원 배열([][])이 필요했다.
    하지만 Python은 이차원 배열([][])을 지원하지 않아서 이중 리스트로 해결했다.([[]])
    함수 search_exist_list(list_name)를 Matrix라는 이중 리스트를 활용한 연결성 여부 함수로 완전히 내용을 바꾸면서,
    
    input:
    10 2 10
    1 2
    9 10
    
    일 때, 연결되지 않은 노드까지 출력하는 문제를 해결하였다.
    
    이제 DFS를 고칠 차례!
    
    
    + search_exist_list(list_name)함수 내에서 48번 라인만 실행하는 경우, key에 None이 저장되는 경우를 어떻게 깔끔하게 다듬을지 모르겠다. 일단 82~83번 라인으로 문제를 잡았다.

--- Baekjoon Online Judge/1260_DFS and BFS.py
+++ Baekjoon Online Judge/1260_DFS and BFS.py
@@ -5,17 +5,22 @@ V = data1[2]
 
 DFS = {}
 BFS = {}
+# 연결성 여부를 확인하는 Matrix
+# 모든 원소가 0인 (N+1)x(N+1)리스트 생성, 0부터 시작하여 N+1개(1부터 시작하는 노드 번호를 위해서),
+Matrix = [[0]*(N+1) for i in range(N+1)] #Python은 이차원배열 대신 이중리스트를 사용한다.
 
-for i in range(1, N+1):
+for i in range(1, N + 1):
     DFS[i] = list()
     BFS[i] = list()
-    
+
 for j in range(0, M):
     data2 = list(map(int, input().split()))
     DFS[data2[0]].append(data2[1])
     DFS[data2[1]].append(data2[0])
     BFS[data2[0]].append(data2[1])
     BFS[data2[1]].append(data2[0])
+    Matrix[data2[0]][data2[1]] = 1
+    Matrix[data2[1]][data2[0]] = 1
 
 # DFS = BFS = dict # 모두 같은 id를 가지는 복사 방법. 하나를 변경하면 모두 똑같이 적용됨.
 '''
@@ -23,73 +28,80 @@ for j in range(0, M):
 # (+) 다른 id로 복사했는데도 서로 영향을 미친다.
 DFS = {}
 BFS = {}
-for k, v in dict.items():
+for k, v in dict.items():  
     DFS[k]=v
     BFS[k]=v
 '''
 
+
 # 공통 함수_1
 def delete_from_list(list_name, num):
-    for i in range(1, N+1):
+    for i in range(1, N + 1):
         if num in list_name[i]:
             list_name[i].remove(num)
 
+
 # 공통 함수_2
-def search_exist_list(list_name):
-    count = 0
+def search_exist_list(num):
     for i in range(1, N+1):
-        if list_name[i]:
-            return i
-        else :
-            count = count+1
-    if count==N :
-        return 0 #전체 종료
-            
-def check_relation(list_name, num):
-    for i in range(1, N+1):
-        if num in list_name[i]:
+        if i in result:
+            continue
+        else:
+            if Matrix[num][i] == 1:
+                return i
+            else:
+                return 0
+
+
+def check_relation(num):
+    for i in range(1, N + 1):
+        if Matrix[num][i]==1 :
             return "OK"
-    return "NO"
-    
+    return 0
+
+
 ############### DFS ###############
 def DFS_(first):
-    if(first==0):
-       quit
+    if (first == 0):
+        quit
     main_key = first
-    print('\nDFS:',DFS)
-    if check_relation(DFS, main_key) == "OK":
-        delete_from_list(DFS, main_key)
+    delete_from_list(DFS, main_key)
+    if check_relation(main_key) == "OK":
         if DFS[main_key]:
             return min(DFS[first])
-        else :
-            return search_exist_list(DFS)
-    else :
+        else:
+            return search_exist_list(main_key)
+    else:
         return 0
 
 
-key=V
-print(key, end=' ')
-while (DFS_(key)) :   # key로 바꿔보자.
+key = V
+result = [key]
+for i in range(N):
     key = DFS_(key)
-    if key!=0 :
-        print(key, end=' ')
+    if key == None: #'None'(=문자열) 아님. None으로 입력해야 한다.
+        break
+    elif key != 0:
+        result.append(key)
 
-print()
+print(*result, sep=" ")
+print('\n')
 '''
 ############### BFS ###############
-
 def BFS_(first):
-    print(first, end=' ')
+    result.append(first)
     delete_from_list(BFS, first)
     if BFS[first]:
         while (BFS[first]) :
             minimum = min(BFS[first])
-            print(minimum, end=' ')
+            print(minimum, end=' ')5
             delete_from_list(BFS, minimum)
             BFS[minimum] = list()
     return search_exist_list(BFS)
-
+    
 key=V
+result = [key] #need?
 while (key != 0):
     key = BFS_(key)
+print(result)
 '''
