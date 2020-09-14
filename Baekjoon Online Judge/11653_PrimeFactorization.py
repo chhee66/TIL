d00c166814765cd60d8c9e365c867fde404e66ff
Chohui Park <chhee66@naver.com>
2020-09-15 02:58:58 +0900

    [EDIT] 11653_PrimeFactorization.py
    https://www.acmicpc.net/problem/11653
    
    그동안 다 [UPDATE]였는데, [EDIT]으로 써와서 [EDIT]으로 통일시켰다.
    
    결국 남의 코드로 성공했다.
    내 코드들은 모두 잘 동작했지만 시간초과가 났다.
    
    하지만 위 코드 또한 오래 걸리긴 마찬가지다.
    그래도 내 코드보다 복잡하지도 않은데 더 빠르다니..ㅜ
    
    RESULT: 맞았습니다!!

--- Baekjoon Online Judge/11653_PrimeFactorization.py
+++ Baekjoon Online Judge/11653_PrimeFactorization.py
@@ -1,24 +1,9 @@
 import sys
-n = int(sys.stdin.readline())
-prime = []
-
-def isPrime(a):
-    if a<2: # 0, 1은 소수가 아니다.
-        return False
-    for i in range(2, a): # a=2일때 동작x, 따라서 2는 True
-        if a%i == 0:
-            return False
-    return True
-
-for i in range(n+1):
-    if isPrime(i):
-        prime.append(i)
-
-for p in prime:
-    '''
-    if n==1:
-        break
-    '''
-    while n!=1 and n%p == 0:
-        print(p)
-        n = int(n / p)
+v = int(sys.stdin.readline())
+i=2
+while v!=1 :
+    if v%i == 0:
+        v = v/i
+        print(i)
+    else :
+        i+=1
