3c5d5784248c93dffae778789457b8342dde6cd4
Chohui Park <chhee66@naver.com>
2020-09-19 16:32:13 +0900

    [COMPLETE] 1260_DFS and BFS.py
    맞았습니다!!

--- Baekjoon Online Judge/1260_DFS and BFS.py
+++ Baekjoon Online Judge/1260_DFS and BFS.py
@@ -1,128 +1,38 @@
-data1 = list(map(int, input().split()))
-N = data1[0]
-M = data1[1]
-V = data1[2]
+# 대박! 나 혼자 풀었는데, 전보다 더 빠르게 풀었다!
+import sys
+from collections import deque
 
-DFS = {}
-BFS = {}
-# 연결성 여부를 확인하는 Matrix
-# 모든 원소가 0인 (N+1)x(N+1)리스트 생성, 0부터 시작하여 N+1개(1부터 시작하는 노드 번호를 위해서),
-Matrix = [[0]*(N+1) for i in range(N+1)] #Python은 이차원배열 대신 이중리스트를 사용한다.
+n, m, v = map(int, sys.stdin.readline().split())
+matrix = [[] for _ in range(n+1)]
 
-for i in range(1, N + 1):
-    DFS[i] = list()
-    BFS[i] = list()
+for i in range(m):
+    x, y = map(int, sys.stdin.readline().split())
+    matrix[x].append(y)
+    matrix[y].append(x)
+for j in range(1, n+1):
+    matrix[j].sort()
 
-for j in range(0, M):
-    data2 = list(map(int, input().split()))
-    DFS[data2[0]].append(data2[1])
-    DFS[data2[1]].append(data2[0])
-    BFS[data2[0]].append(data2[1])
-    BFS[data2[1]].append(data2[0])
-    Matrix[data2[0]][data2[1]] = 1
-    Matrix[data2[1]][data2[0]] = 1
-
-# DFS = BFS = dict # 모두 같은 id를 가지는 복사 방법. 하나를 변경하면 모두 똑같이 적용됨.
-'''
-# dictionary를 다른 id로 복사하는 방법
-# (+) 다른 id로 복사했는데도 서로 영향을 미친다.
-DFS = {}
-BFS = {}
-for k, v in dict.items():  
-    DFS[k]=v
-    BFS[k]=v
-'''
+visit = []
+def DFS(start, visit):
+    visit.append(start)
+    for i in matrix[start]:
+        if i not in visit:
+            visit = DFS(i, visit)
+    return visit
 
 
-# 공통 함수
-def delete_from_list(list_name, num):
-    for i in range(1, N + 1):
-        if num in list_name[i]:
-            list_name[i].remove(num)
+def BFS(start) :    # matrix 정렬 필요
+    visit = []
+    q = deque()
+    q.append(start)
+    visit.append(start)
+    while len(q)!=0 :
+        z = q.popleft()
+        for e in matrix[z]:
+            if e not in visit :
+                q.append(e)
+                visit.append(e)
+    return visit
 
-
-def check_relation(num):
-    for i in range(1, N + 1):
-        if Matrix[num][i] == 1 :
-            return "OK"
-    return 0
-
-# DFS
-def search_exist_list_for_DFS(num):
-    for i in range(1, N+1):
-        if i in result:
-            continue
-        else:
-            if Matrix[num][i] == 1:
-                return i
-            else:
-                return 0
-''' # 이렇게 줄여도 실행결과가 같나?
-    for i in range(1, N+1):
-        if (i not in result) and (Matrix[num][i] == 1):
-            return i
-    return 0
-'''
-
-
-############### DFS ###############
-def DFS_(first):
-    if (first == 0):
-        quit
-    main_key = first
-    delete_from_list(DFS, main_key)
-    if check_relation(main_key) == "OK":
-        if DFS[main_key]:
-            return min(DFS[first])
-        else:
-            return search_exist_list_for_DFS(main_key)
-    else:
-        return 0
-
-
-key = V
-result = [key]
-for i in range(N):
-    key = DFS_(key)
-    if key == None: #'None'(=문자열) 아님. None으로 입력해야 한다.
-        break
-    elif key != 0:
-        result.append(key)
-
-print(*result, sep=" ")
-
-
-# BFS
-def search_exist_list_for_BFS(num):
-    for i in range(1, N+1):
-        if i in result:
-            continue
-        elif Matrix[num][i] == 1:
-            return is_it_min(BFS, i)
-    return 0
-
-def is_it_min(list_name, i):
-    if i > min(list_name[i]):
-        return min(list_name[i])
-    else:
-        return i
-
-result=[]
-############### BFS ###############
-def BFS_(first):
-    result.append(first)
-    delete_from_list(BFS, first)
-    if BFS[first]:
-        while (BFS[first]) :
-            minimum = min(BFS[first])
-            result.append(minimum)
-            delete_from_list(BFS, minimum)
-            BFS[minimum] = list()
-    return search_exist_list_for_BFS(minimum)
-    
-key=V
-while (key != 0):
-    key = BFS_(key)
-    if key == None:
-        break
-print(*result, sep=" ")
+print(*DFS(v, visit))
+print(*BFS(v))
